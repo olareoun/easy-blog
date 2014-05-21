fe1e88cf74c529d151103995d8b36d97d6d6cf52
olareoun <victor.miguez.martinez@gmail.com>
2014-05-21 10:21:34 +0200

    cleaning up

--- web/lib/base_post.rb
+++ web/lib/base_post.rb
@@ -1,43 +1,43 @@
 module BasePost
 
-    def entitle aTitle
-      @title = aTitle
-    end
+  def entitle aTitle
+    @title = aTitle
+  end
 
-    def putContent aContent
-      @content = aContent
-    end
+  def putContent aContent
+    @content = aContent
+  end
 
-    def putImages images
-      @images = images
-    end
+  def putImages images
+    @images = images
+  end
 
-    def putAudio audio
-      @audio = audio
-    end
+  def putAudio audio
+    @audio = audio
+  end
 
-    def putUpdated updated
-      @updated = Time.at(updated / 1000)
-    end
+  def putUpdated updated
+    @updated = Time.at(updated / 1000)
+  end
 
-    def hasImage?
-      !(@images.nil? || @images.empty?) && @images.first
-    end
+  def hasImage?
+    !(@images.nil? || @images.empty?) && @images.first
+  end
 
-    def hasTitle
-      !@title.nil? && !@title.empty?
-    end
+  def hasTitle
+    !@title.nil? && !@title.empty?
+  end
 
-    def hasContent
-      !@content.nil? && !@content.empty? && !contentWithoutHtmlTags.empty?
-    end
+  def hasContent
+    !@content.nil? && !@content.empty? && !contentWithoutHtmlTags.empty?
+  end
 
-    def contentWithoutHtmlTags
-      @content.gsub(%r{</?[^>]+?>}, '')
-    end
+  def contentWithoutHtmlTags
+    @content.gsub(%r{</?[^>]+?>}, '')
+  end
 
-    def empty?
-      to_s.empty?
-    end
+  def empty?
+    to_s.empty?
+  end
 
 end
